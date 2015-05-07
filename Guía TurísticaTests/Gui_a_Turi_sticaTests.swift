0a451498555d6e2a3d330e44a8a82e0a2fb14ac9
Pablo Pasqualino <github@osmosisdc.com>
2015-05-07 01:43:23 -0300

    Initial Commit

---
+++ Guía TurísticaTests/Gui_a_Turi_sticaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Gui_a_Turi_sticaTests.swift
+//  Guía TurísticaTests
+//
+//  Created by Pablo Pasqualino on 5/7/15.
+//  Copyright (c) 2015 Pablo Pasqualino. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Gui_a_Turi_sticaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
